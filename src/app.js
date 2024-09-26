da60f0aab430c5dccedb317793e861c27abba35e
Nicolas Perez <nbome12@outlook.com>
2024-09-26 08:30:22 -0500

    creacion de la vista crearUsuario usando ejs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import {join, dirname} from 'path'
 import { fileURLToPath } from 'url';
 import usuariosRoutes from './routes/usuarios.route.js';
 import { logErrors, errorHandler } from '../middlewares/error.handler.js';
+import expressLayouts from 'express-ejs-layouts';
 
 //Inicializaciones
 const app =  express();
@@ -12,9 +13,33 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 //ajustes
 app.set('port', process.env.PORT || 3000);
 
+//handlebars config
+/*app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');*/
+app.use(expressLayouts);
+//definir el directorio
+app.set('layout', 'layouts/layout');
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views'));
+
 //rutas
 //app.get('/api/v1/usuarios', usuariosRoutes);
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/api/v1/crearUsuario', (req, res) => {
+    res.render('crearUsuario', { layout: './layouts/sidebar' });
+});
+
 app.use('/api/v1/usuarios', usuariosRoutes);
+
 
 //middlewares
 app.use(morgan('dev'));
