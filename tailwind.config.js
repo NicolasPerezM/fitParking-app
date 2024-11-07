e0511b76524a1315a54a879aead77ba5234399bf
Nicolas Perez <nbome12@outlook.com>
2024-11-07 15:25:41 -0500

    cambios en el frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,12 @@ export default {
     extend: {
       backgroundImage: {
         'general': "url('/images/background.png')",
-        'login-img': "url('/images/login-imageBackground.jpeg')"
+        'login-img': "url('/images/login-imageBackground.jpeg')",
+        'logo-fitParking': "url('/images/logoFitP.png')",
       },
       colors: {
         'main-yellow': '#F6C90E',
-        'main-black': '#303841',
+        'main-black': '#1f2937 ',
         'secundary-light': '#EEEEEE',
         'secundary-dark': '#3A4750',
       }
